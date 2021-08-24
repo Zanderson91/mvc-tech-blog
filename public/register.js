1feb9db73dafebf345a006157d274fc18582e7cb
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-24 07:24:41 -0500

    Created conditional statements for username/email/password. A user will now be able to create and account to access the dashboard

--- public/register.js
+++ public/register.js
@@ -4,3 +4,22 @@ const signupFormHandler = async (event) => {
 const name = document.querySelector('#name-register').value.trim();
 const email = document.querySelector('#email-register').value.trim();
 const password = document.querySelector('#password-register').value.trim();
+
+if (name && email && password) {
+        const response = await fetch('/api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
