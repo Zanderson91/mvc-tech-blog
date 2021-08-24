6cc90eac7ddc806afb133765b18d9866a1216b7a
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-24 07:27:30 -0500

    Await function to organize data from user

--- public/createPost.js
+++ public/createPost.js
@@ -1,6 +1,12 @@
 
 const addPost = async (event) => {
     event.preventDefault();
-    
+
 const title = document.querySelector('#post-title').value.trim();
 const content = document.querySelector('#post-content').value.trim();
+
+const response = await fetch('/api/post', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+    });
