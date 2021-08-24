e5889cf9a702130ef32f873cb8cb46f3238fdb4a
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-24 07:38:22 -0500

    Created variables for title, content and id...WIP using location.toString() to return correcr url

--- public/updatePost.js
+++ public/updatePost.js
@@ -1,14 +1,11 @@
+const editPost = async (event) => {
+    event.preventDefault();
 
-
-
-
-
-
-
-
-
-
-
+const title = document.querySelector('#post-title').value.trim();
+const content = document.querySelector('#post-content').value.trim();
+const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
 
 
 const response = await fetch(`/api/post/${id}`, {
