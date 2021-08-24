083e7a1140e0a91812d6386adabf04246e8c5fc9
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-24 07:35:22 -0500

    Working backwards on this one.. created put request and conditional statements..but need querey selectors for variables

---
+++ public/updatePost.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+const response = await fetch(`/api/post/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editPost);
