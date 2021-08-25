15a5efedd179c448eb6c0e9fb8acf741185605fa
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-25 07:23:54 -0500

    same render info as previous but created a logged-in req.session

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,3 +41,13 @@ router.get('/post/:id', async (req, res) => {
                 },
             ],
         });
+            const post = postData.get({ plain: true });
+
+        res.render('single-post', {
+            post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
