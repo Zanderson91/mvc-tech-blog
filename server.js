cdc3368d1de8fcf342f69e4bb40d92b2394534f0
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-25 08:23:05 -0500

    creating cookies via hbs/sess

--- server.js
+++ server.js
@@ -6,4 +6,24 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+
+
+const hbs = exphbs.create({ helpers });
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+        expires: 5 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
