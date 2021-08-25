7e741c8561ad6e1ea916aac2614a4340542ce960
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-25 07:18:56 -0500

    WIP..creating GET route to get all post data by user name

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
