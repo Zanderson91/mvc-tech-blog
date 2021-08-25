538ad90486e38474117a9dc78f33322d1031d110
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-25 07:07:15 -0500

    Working through postRoutes..set variables and created first post route.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -43,7 +43,19 @@ router.post('/login', async (req, res) => {
             res.json({ user: userData, message: 'You are now logged in!' });
         });
 
-    } catch (err) {
+        } catch (err) {
         res.status(400).json(err);
     }
-});        
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
