896ec3466cd8d777f316f55f6606a5ac6bcdc508
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-25 07:25:00 -0500

    Used same login/logout get routes to ensure that users be redirected to login or signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,3 +51,25 @@ router.get('/post/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('signup');
+});
+
+
+
+module.exports = router;
