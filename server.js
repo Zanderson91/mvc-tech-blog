09aee54b3864f8a7b5c2c5c4c6486afb35fa79a9
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-25 08:21:26 -0500

    created variables for requiring

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
