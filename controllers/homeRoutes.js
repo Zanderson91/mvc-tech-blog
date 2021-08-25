73eb4b0e0882ed499b3e39dd1eea91d288848bde
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-25 07:20:18 -0500

    used map function for individual post and render function for homepage to begin to map data to page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,3 +11,14 @@ router.get('/', async (req, res) => {
                 },
             ],
         });
+        
+const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
